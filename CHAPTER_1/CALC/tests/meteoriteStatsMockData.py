48c23b6ae1330b30d83d965d82ed79370a0ae1f4
Sigma247 <khalildida93@gmail.com>
2023-01-16 22:03:58 +0100

    all chapters

---
+++ CHAPTER_1/CALC/tests/meteoriteStatsMockData.py
@@ -0,0 +1,39 @@
+mock_data = [
+        {
+            "name": "Aachen",
+            "id": "1",
+            "nametype": "Valid",
+            "recclass": "L5",
+            "mass": "21",
+            "fall": "Fell",
+            "year": "1880-01-01T00:00:00.000",
+            "reclat": "50.775000",
+            "reclong": "6.083330",
+            "geolocation": {
+            "type": "Point",
+            "coordinates": [
+                6.08333,
+                50.775
+            ]
+            }
+        },
+        {
+            "name": "Aarhus",
+            "id": "2",
+            "nametype": "Valid",
+            "recclass": "H6",
+            "mass": "720",
+            "fall": "Fell",
+            "year": "1951-01-01T00:00:00.000",
+            "reclat": "56.183330",
+            "reclong": "10.233330",
+            "geolocation": {
+            "type": "Point",
+            "coordinates": [
+                10.23333,
+                56.18333
+            ]
+            }
+        }
+    ]
+    
---
+++ CHAPTER_2/tests/serializers/test_room_json_serializer.py
@@ -0,0 +1,24 @@
+import json
+import uuid
+
+from rentomatic.domain import room as r
+from rentomatic.serializers import room_json_ser
+
+
+def test_serializer_domain_room():
+    
+    code=uuid.uuid4()
+    room = r.Room(code=code, size=200, price=10, longitude=-0.09983338, latitude=52.7477362)
+    room_json_ser_expected_output = """
+    {{
+        "code": "{}",
+        "size": 200,
+        "price": 10,
+        "longitude": -0.09983338,
+        "latitude": 52.7477362
+    }}
+    """.format(code)
+    
+    json_room = json.dumps(room, cls=room_json_ser.RoomJsonEncoder)
+    assert json.loads(json_room) == json.loads(room_json_ser_expected_output)
+
