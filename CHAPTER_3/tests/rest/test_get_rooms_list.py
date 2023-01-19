44eb45bbcfaa86859c180979584686820c6751eb
Sigma247 <khalildida93@gmail.com>
2023-01-19 23:13:37 +0100

    test_get_with_filters

--- CHAPTER_3/tests/rest/test_get_rooms_list.py
+++ CHAPTER_3/tests/rest/test_get_rooms_list.py
@@ -28,4 +28,19 @@ def test_get(mock_use_case, client):
     assert args[0].filters == {}
     assert http_response.status_code == 200
     assert http_response.mimetype == 'application/json'
+
+@mock.patch('rentomatic.use_case.room_list_use_case.RoomListUseCase')
+def test_get_with_filters(mock_use_case, client):
+    mock_use_case().execute.return_value = SuccessResponseBuilder()\
+                                            .set_value(rooms)\
+                                            .set_response_message_and_build('Rooms List Data')\
+                                            .get_response()
+    http_response = client.get('/rooms?filter_price__get=2&filter_price__lt=6')
+    assert json.loads(http_response.data.decode('UTF-8')) == {'type': 'Success', 'message': 'Rooms List Data', 'response': [room_dict]}
     
+    mock_use_case().execute.assert_called()
+    args, kwargs = mock_use_case().execute.call_args
+    assert args[0].filters == {'price__gt': '2', 'price__lt': '6'}
+    
+    assert http_response.status_code == 200
+    assert http_response.mimetype == 'application/json'
